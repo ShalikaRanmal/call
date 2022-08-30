2232ca1c6fa8cbb7b3d25272a55421001bd9b256
Shalika Ranmal <dgshranmal@gmail.com>
2022-08-31 00:48:04 +0530

    s

---
+++ src/com/calculateArea/circle.java
@@ -0,0 +1,23 @@
+package com.calculateArea;
+import java.lang.Math.*;
+
+public class circle {
+    public circle(int x){
+        radius= x;
+    }
+
+    public int getRadios() {
+        return radius;
+    }
+
+    public void setRadios(int radius) {
+        this.radius = radius;
+    }
+
+
+    private int radius;
+
+    public double calculateArea(){
+        return Math.PI*radius*radius ;
+    }
+}
